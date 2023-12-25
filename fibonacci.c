73af170d4b38820c055e03aae8ca641c346d1dcf
VidnyaniU <vidnyani.umathe@gmail.com>
2023-12-25 17:52:43 +0530

    Initial commit

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+    int n = 6;
+    // printf("Fibonacci of %d is %d.\n", n, fib(n));
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fib(i));
+    }
+
+    return 0;
+}
