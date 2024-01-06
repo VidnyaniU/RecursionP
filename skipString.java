7f76b4e1c09a9d089f94df5182869d588b7aafdb
VidnyaniU <vidnyani.umathe@gmail.com>
2024-01-06 19:06:08 +0530

    updated skipString.java
    Added another function to skip certain string if its not the required string.

--- skipString.java
+++ skipString.java
@@ -1,6 +1,8 @@
 public class skipString {
     public static void main(String[] args) {
         System.out.println(skipStr("bananaapplepineapple"));
+        System.out.println(skipAppNotApple("bananaapplepineapp"));
+
     }
 
     static String skipStr(String up) {
@@ -13,6 +15,20 @@ public class skipString {
             return skipStr(up.substring(5)); // skipping the character 'a' and passing th leftover string
         } else {
             return up.charAt(0) + skipStr(up.substring(1)); // concatenating the characters which are not 'a'
+        }
+    }
+
+    // skip app but when it is apple don't
+    static String skipAppNotApple(String up) {
+        if (up.isEmpty()) // base condition
+        {
+            return "";
+        }
+
+        if (up.startsWith("app") && !up.startsWith("apple")) {
+            return skipAppNotApple(up.substring(3));
+        } else {
+            return up.charAt(0) + skipAppNotApple(up.substring(1)); // concatenating the characters
         }
     }
 }
