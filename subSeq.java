ebcea26dbc56147021747811fd6a849cc5ce120c
VidnyaniU <vidnyani.umathe@gmail.com>
2024-01-09 10:35:12 +0530

    Updated subSeq.java
    Added  function to print subsequence with ascii value.

--- subSeq.java
+++ subSeq.java
@@ -1,6 +1,8 @@
 public class subSeq {
     public static void main(String[] args) {
-        subSequence("", "abc");
+        // subSequence("", "abc");
+
+        subSequenceAscii("", "abc");
     }
 
     static void subSequence(String processed, String unprocessed) {
@@ -13,5 +15,21 @@ public class subSeq {
         // recursion calls
         subSequence(processed + ch, unprocessed.substring(1));// take it or
         subSequence(processed, unprocessed.substring(1));// leave it
+    }
+
+    // printing subsequences with ascii value
+    static void subSequenceAscii(String processed, String unprocessed) {
+        // base condition
+        if (unprocessed.isEmpty()) {
+            System.out.print(processed + " ");
+            return;
+        }
+        char ch = unprocessed.charAt(0);
+        // recursion calls
+        subSequenceAscii(processed + ch, unprocessed.substring(1));// take it or
+        subSequenceAscii(processed, unprocessed.substring(1));// leave it
+        subSequenceAscii(processed + (ch + 0), unprocessed.substring(1));// need another recursion call for ascii and
+                                                                         // ch+0 gives ascii value
+
     }
 }
